f55444722bfd75898285b61b1a280677a5fd18ca
FadyTawadrous <40478872+FadyTawadrous@users.noreply.github.com>
2026-07-10 16:28:36 +0300

    Create project structure

---
+++ src/app/shared/components/drop-zone/drop-zone.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-zone',
+  imports: [],
+  templateUrl: './drop-zone.html',
+  styleUrl: './drop-zone.css',
+})
+export class DropZone {
+
+}
---
+++ src/app/shared/components/format-selector/format-selector.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-format-selector',
+  imports: [],
+  templateUrl: './format-selector.html',
+  styleUrl: './format-selector.css',
+})
+export class FormatSelector {
+
+}
